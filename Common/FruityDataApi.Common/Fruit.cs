e97222f86390345d8e1ba5046146512029e948e4
Mampazhassery <brahmadath.mampazhassery@capgemini.com>
2023-05-07 00:16:05 +0530

    Initial commit of solution's initial state

---
+++ Common/FruityDataApi.Common/Fruit.cs
@@ -0,0 +1,34 @@
+﻿namespace FruityDataApi.Common;
+public class Fruit
+{
+    public int Id {get; set;}
+
+    public string Name {get; set;}
+
+    public string Family {get; set;}
+
+    public string Order {get; set;}
+
+    public string Genus {get; set;}
+
+    public Nutrition Nutritions {get; set; }
+
+    public Fruit()
+    {
+        Nutritions = new Nutrition();
+    }
+}
+
+public class Nutrition
+{
+    public int Calories { get; set; }
+
+    public decimal Fat { get; set; }  
+
+    public decimal Sugar { get; set; }
+
+    public decimal Carbohydrates { get; set; }
+
+    public decimal Protien { get; set; }
+    
+}
---
+++ Services/FruityDataApi.Services/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace FruityDataApi.Services;
+public class Class1
+{
+
+}
